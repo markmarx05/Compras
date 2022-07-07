7711f981cc312f065f9d7714a2e044193a676534
Marcelo Marques <markmarx05@gmail.com>
2022-07-07 07:07:05 -0300

    update

--- src/components/ListItem/index.tsx
+++ src/components/ListItem/index.tsx
@@ -17,7 +17,7 @@ export const ListItem = ({ item, onChange }: Props) => {
                 checked={item.done}
                 onChange={e => onChange(item.id, e.target.checked)}
             />
-            <label>{item.name} - {item.done.toString()}</label>
+            <label>{item.name}</label>
         </C.Container>
     );
 }
